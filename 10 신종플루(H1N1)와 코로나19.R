543550b6cbb52288f072c97000cdd4c835114730
cysics <cysics@gmail.com>
2020-03-13 10:18:18 +0900

    3월 9일 데이터까지만 적용

--- 10 신종플루(H1N1)와 코로나19.R
+++ 10 신종플루(H1N1)와 코로나19.R
@@ -1,5 +1,6 @@
 #### 1. 데이터 전처리 ####
-library(tidyverse); library(reshape2); library(tibble)
+library(tidyverse); library(reshape2); library(tibble); 
+library(ggpubr); library(gganimate); 
 rdata <- list()
 
 #### 가. 신종플루 데이터 ####
@@ -17,14 +18,13 @@ rdata$url <- c("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master
          "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv", 
          "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
 
+# 미국은 50개 주 데이터만 인정, 후베이는 따로 구분하지 않음.
 rdata$confirmedCases <- read_csv(rdata$url[1]) %>% select(-c(Lat,Long)) %>% 
   melt(id=c('Country/Region','Province/State')) %>% 
   rename("Country"=1, "State"=2, "Variable"=3, "Confirmed"=4) %>% 
   mutate(Country=ifelse(Country=="US" & !grepl(",", State), NA, Country)) %>% 
   group_by(Country,Variable) %>% summarise(Confirmed=sum(Confirmed)) %>% 
   rename("Country"=1,"Date"=2,"Confirmed"=3)
-
-nrow(rdata$confirmedCases %>% filter(Country=="US"))
 
 rdata$DeathCases <- read_csv(rdata$url[2]) %>% select(-c(Lat,Long)) %>% 
   melt(id=c('Country/Region','Province/State')) %>% 
@@ -55,29 +55,48 @@ max(rdata$COVID19$Date)
 
 #### 2. 데이터 분석 ####
 #### 가. 가장 최근 날짜 사망률과 비교하기 ####
-rdata$data1 <- rdata$COVID19 %>% select(c("Country", "DeathRate", "Date")) %>% 
+rdata$data1 <- rdata$COVID19 %>% 
+  select(c("Country", "Confirmed", "Deaths", "DeathRate", "Date")) %>% 
   filter(Country=="Korea" | Country=="China" | Country=="Japan" | Country=="US")
 
-rdata$data2 <- rdata$H1N1 %>% select(c("Country", "DeathRate"))
+rdata$data2 <- rdata$H1N1 %>% select(c("Country", "Confirmed", "Deaths", "DeathRate"))
 
 str(data <- rbind(data.frame(group="COVID19", rdata$data1), 
   data.frame(group="H1N1", merge(rdata$data2, rdata$data1['Date'], by=NULL))) %>%
       unique() %>% mutate(DeathRate=round(DeathRate*100, 2)) %>% 
       mutate(group=factor(group, levels=c("H1N1", "COVID19"))) %>% 
-      mutate(Country=factor(Country, levels=c("Korea", "China", "Japan", "US"))))
+      mutate(Country=factor(Country, levels=c("Korea", "China", "Japan", "US"))) %>% 
+  filter(Date<"2020-03-10"))
 
-library(ggpubr)
-data[data$Date=="2020-03-11",] %>% ggbarplot(x="Country", y="DeathRate", 
-  ylab="Death Rate(%)", fill="group", ylim=c(0,4.2), palette="Paired",
+data[data$Date=="2020-03-09",] %>% ggbarplot(x="Country", y="DeathRate", 
+  ylab="Death Rate(%)", fill="group", ylim=c(0,4.5), palette="Paired",
   position=position_dodge(0.85), label=T, lab.size=5) + 
   theme(text=element_text(size=20), 
   plot.margin=margin(10, 30, 10, 10), legend.position=c(0.2, 0.7))
 
 
-#### 나. 시간에 따른 변화 ####
-library(gganimate)
-data %>% ggbarplot(x="Country", y="DeathRate", fill="group", ylab="Death Rate(%)", 
-  palette="Paired", position=position_dodge(0.85), label=T, lab.size=7)+
-  theme(text=element_text(size=20), plot.margin=margin(10, 30, 10, 10),
-        legend.position=c(0.2, 0.7))+
-  transition_time(Date) + labs(title = "{frame_time}")
+#### 나. 시간에 따른 사망률 비교 ####
+print(result <- data %>% ggplot(aes(x=Country, y=DeathRate, fill=group)) + 
+  geom_bar(stat="identity", color="black", position=position_dodge(0.9))+
+  geom_text(aes(label=paste0(DeathRate)), vjust=-0.5, 
+            position=position_dodge(0.9), size=6)+
+  theme(text=element_text(size=20), 
+  plot.margin=margin(10, 10, 10, 10), legend.position=c(0.2, 0.7)) +
+  transition_time(Date) + labs(title = "{frame_time}"))
+
+animate(result, 300, fps=10, duration=30, end_pause=50, width=500, height=400, 
+        renderer=gifski_renderer("H1N1vsCOVID19_DR.gif"))
+
+
+#### 다. 확진자 수 비교 ####
+print(result <- data %>% ggplot(aes(x=Country, y=Confirmed, fill=group)) + 
+  geom_bar(stat="identity", color="black", position=position_dodge(0.8))+
+  coord_cartesian(ylim=c(0, max(data$Confirmed)+10000))+
+  scale_y_continuous(labels=scales::comma) + 
+  geom_text(aes(label=paste0(scales::comma(Confirmed))), vjust=-0.5, 
+            position=position_dodge(0.8), size=6) + theme(legend.position="top") +
+  theme(text=element_text(size=20), plot.margin=margin(10, 10, 10, 10))+
+  transition_time(Date) + labs(title = "{frame_time}"))
+
+animate(result, 300, fps=10, duration=30, end_pause=50, width=500, height=400, 
+        renderer=gifski_renderer("H1N1vsCOVID19_CM.gif"))
